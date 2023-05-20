a07bfc5f89e1f10cc275e9250b7fb424d13e16f4
salmanwk34 <102039344+salmanwk34@users.noreply.github.com>
2023-05-20 19:50:48 +0200

    Update googleAuth.js

--- googleAuth.js
+++ googleAuth.js
@@ -1,8 +1,8 @@
 const { google } = require('googleapis');
 
 const oauth2Client = new google.auth.OAuth2(	 
-"127222647756-l6n7p1le8muepatkuitv56625k9sblbf.apps.googleusercontent.com",
-"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",
+"**********************************************************************",
+"*********************************************************************",
 "http://localhost:3000/auth/google/callback"
 
 );
